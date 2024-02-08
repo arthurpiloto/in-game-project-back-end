3796abc13864f30c911720be9ae397cdce095560
Arthur Piloto <arthurpiloto.github@gmail.com>
2024-02-08 08:21:14 -0300

    creating a request intercepter

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,15 @@ import express from 'express';
 import cors from 'cors';
 import https from 'https';
 import http from 'http';
+import { requestIntercepter } from "./utils/requestIntercepter";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.all('*', requestIntercepter)
 
 const runServer = (port: number, server: http.Server) => {
     server.listen(port, () => {
