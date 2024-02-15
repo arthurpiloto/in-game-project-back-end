11dd26b33d4052e12936111fc9aa86e79465da4b
Arthur Piloto <arthurpiloto.github@gmail.com>
2024-02-15 12:33:16 -0300

    15/02 progress

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from "express";
-import { z } from "zod";
+import { date, z } from "zod";
 import * as user from '../models/DAO/user';
+import { MESSAGE_ERROR, MESSAGE_SUCCESS } from "../utils/config";
 
 export const addUser: RequestHandler = async (req, res) => {
     const addUserSchema = z.object({
@@ -9,13 +10,22 @@ export const addUser: RequestHandler = async (req, res) => {
         senha: z.string(),
         id_posicao: z.number().min(1).max(6),
         id_sexo: z.number().min(1).max(2),
-        data_nasc: z.date(),
+        data_nasc: z.coerce.date(),
         foto: z.string()
     })
     const body = addUserSchema.safeParse(req.body);
-    if (!body.success) return res.json({ error: 'Invalid data' });
+    if (!body.success) return res.json({ error: MESSAGE_ERROR.INVALID_DATA });
 
-    // const newUser = await user.insertUser({
+    const newUser = await user.insertUser({
+        nome: body.data.nome,
+        email: body.data.email,
+        senha: body.data.senha,
+        id_posicao: body.data.id_posicao,
+        id_sexo: body.data.id_sexo,
+        data_nasc: body.data.data_nasc,
+        foto: body.data.foto
+    });
+    if (newUser) return res.status(201).json({ message: MESSAGE_SUCCESS.INSERT_ITEM });
 
-    // });
+    return res.status(500).json({ error: MESSAGE_ERROR.INTERNAL_ERROR });
 }
--- src/models/DAO/user.ts
+++ src/models/DAO/user.ts
@@ -2,11 +2,18 @@ import { PrismaClient, Prisma } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-type User = { nome: string, email: string, senha: string, id_posicao: number, id_sexo: number, data_nasc: Date, foto?: string }
+type User = { nome: string, email: string, senha: string, id_posicao: number, id_sexo: number, data_nasc: Date, foto: string | undefined }
 export const insertUser = async (data: User) => {
     try {
-        let sql = `INSERT INTO tbl_user`
+        let sql = `INSERT INTO tbl_user (nome, email, senha, id_posicao, id_sexo, data_nasc, foto) 
+        VALUES ('${data.nome}', '${data.email}', md5('${data.senha}'), ${data.id_posicao}, ${data.id_sexo}, '${data.data_nasc}', '${data.foto}');`
+
+        const result = await prisma.$executeRawUnsafe(sql);
+
+        if (result) return true;
+        return false;
+
     } catch (err) {
-        return false
+        return false;
     }
 }
