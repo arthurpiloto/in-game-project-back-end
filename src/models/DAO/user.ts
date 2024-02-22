901830672275bf3e5bbbaae5b3391dce37bca29d
Arthur Piloto <arthurpiloto.github@gmail.com>
2024-02-22 09:41:55 -0300

    changing global types

--- src/models/DAO/user.ts
+++ src/models/DAO/user.ts
@@ -1,9 +1,9 @@
 import { PrismaClient } from '@prisma/client';
-import { UserRegister } from '../global';
 import { JsonArray } from '@prisma/client/runtime/library';
 
 const prisma = new PrismaClient();
 
+type UserRegister = { nome: string, email: string, senha: string, id_posicao: number, id_sexo: number, data_nasc: Date, foto: string | undefined }
 export const insertUser = async (data: UserRegister) => {
     try {
         let sql = `INSERT INTO tbl_user (nome, email, senha, id_posicao, id_sexo, data_nasc, foto) 
